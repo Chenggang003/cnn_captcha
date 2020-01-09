0dc16594c0cad0c95789b1428c29f5a888fad7ed
chengang <chengang@vcredit.com>
2020-01-09 10:02:49 +0800

    从线上的地址下载图片验证码到本地目录

--- online_captcha_save_local.py
+++ online_captcha_save_local.py
@@ -5,7 +5,7 @@ import json
 import requests
 import os
 import time
-
+import base64
 
 def main():
     with open("conf/app_config.json", "r") as f:
@@ -19,12 +19,12 @@ def main():
                       "Chrome/65.0.3325.146 Safari/537.36",
     }
 
-    for index in range(200):
+    for index in range(1):
         # 请求
         while True:
             try:
-                response = requests.request("GET", "http://www.sh.10086.cn/service/server/servlet/validateCodeServlet"
-                                                   "?width=300&height=100&fontSize=60&rnd=1578292412588",
+                response = requests.request("GET", "http://zxgk.court.gov.cn/shixin/captchaNew.do?captchaId"
+                                                   "=U3qUb6wgg93Hcs9TDXm6CFPTc8uL9BM0&random=0.6914391412872873",
                                             headers=headers, timeout=6)
                 if response.text:
                     break
@@ -33,12 +33,21 @@ def main():
             except Exception as ee:
                 print(ee)
 
+        # 识别对应的验证码
+        headers = {
+            'Content-Type': "application/json",
+        }
+        base64_data = base64.b64encode(response.content).decode()
+        params_json = json.dumps({'channelId': '1', 'imageBase64': base64_data})
+        res = requests.post("", data=params_json, headers=headers)
+        print(res.text)
         # 保存文件
-        img_name = "_{}.{}".format(str(time.time()).replace(".", ""), "png")
+        data = json.loads(res.text)
+        img_name = "{}_{}.{}".format(data['result'], str(time.time()).replace(".", ""), "jpg")
         path = os.path.join(origin_dir, img_name)
         with open(path, "wb") as f:
             f.write(response.content)
-        print("============== end ==============")
+    print("============== end ==============")
 
 
 if __name__ == '__main__':
